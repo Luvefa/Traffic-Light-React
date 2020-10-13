870ff35dd96ab342d1d0f87813129c1b4b66fc6b
Luvefa <luis.velazco001@mymdc.net>
2020-10-13 16:57:23 +0000

    first commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,10 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-
-//include bootstrap npm library into the bundle
-import "bootstrap";
-
 //include your index.scss file into the bundle
 import "../styles/index.scss";
 
-//import your own components
-import { Home } from "./component/home.js";
+import TrafficLight from "./component/TrafficLight.js";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<TrafficLight />, document.querySelector("#app"));
